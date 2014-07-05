36d235898209472d51857883a4055324d75f8975
Tobias Haar <tobias.haar@googlemail.com>
2014-07-05 15:46:14 +0100

    installed jsdoc

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,6 +2,16 @@ module.exports = function(grunt) {
 
   // Project configuration.
   grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json')
+    pkg: grunt.file.readJSON('package.json'),
+    jsdoc : {
+      dist : {
+        src: ['engine/src/**/*.js'],
+        options: {
+          destination: 'doc'
+        }
+      }
+    }
   });
+
+  grunt.loadNpmTasks('grunt-jsdoc');
 };
