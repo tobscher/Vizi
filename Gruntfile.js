7d61bed0bffd60c0a686f30035ef14ba9efa5b75
Tobias Haar <tobias.haar@googlemail.com>
2014-07-07 18:11:58 +0100

    added compilation tasks to grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,8 +10,38 @@ module.exports = function(grunt) {
           destination: 'doc'
         }
       }
+    },
+    shell: {
+      compileDebug: {
+        command: [
+          'cd engine/utils',
+          './compile-debug.sh'
+        ].join("&&")
+      },
+      compileMin: {
+        command: [
+          'cd engine/utils',
+          './compile-min.sh'
+        ].join("&&")
+      },
+    },
+    watch: {
+      scripts: {
+        files: ['engine/src/**/*.js'],
+        tasks: ['compile:debug'],
+        options: {
+          spawn: false,
+        },
+      },
     }
   });
 
   grunt.loadNpmTasks('grunt-jsdoc');
+  grunt.loadNpmTasks('grunt-shell');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  grunt.registerTask('compile:debug', ['shell:compileDebug']);
+  grunt.registerTask('compile:min', ['shell:compileMin']);
+  grunt.registerTask('compile:all', ['compile:debug', 'compile:min']);
+  grunt.registerTask('compile', ['compile:all']);
 };
